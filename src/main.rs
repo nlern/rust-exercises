017009b6d0ec2f047d2ce2306bfa25d7d1cf1047
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-10 07:40:10 +0530

    feat: temperature converter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut unit = String::new();
+        println!("Enter input unit(C/c=Celsius, F/f=Fahrenheit).");
+        io::stdin()
+            .read_line(&mut unit)
+            .expect("Failed to read line");
+        let mut t_input = String::new();
+        println!("Enter temperature to convert.");
+        io::stdin()
+            .read_line(&mut t_input)
+            .expect("Failed to read line");
+
+        let t_input: f64 = match t_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid temperature. Please try again");
+                continue;
+            }
+        };
+        let unit = unit.trim().to_lowercase();
+        let (t_out, unit_out) = match unit.as_str() {
+            "c" => {
+                // convert to fahrenheit
+                let t_out = t_input * (9.0 / 5.0) + 32.0;
+                (t_out, "Fahrenheit")
+            }
+            "f" => {
+                // convert to celsius
+                let t_out = (t_input - 32.0) * (5.0 / 9.0);
+                (t_out, "Celsius")
+            }
+            _ => {
+                println!("Invalid temperature unit! Please try again.");
+                continue;
+            }
+        };
+
+        println!("Converted temperature in {}: {:.2}", unit_out, t_out);
+
+        break;
+    }
 }
