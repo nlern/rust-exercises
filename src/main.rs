74ef120af484b5361ac296f689108647d7943dd4
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-10 08:30:46 +0530

    feat: find nth fibonacci number

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     loop {
         println!("exercises");
         println!("1. temp. converter");
+        println!("2. fibonacci");
         println!("0. Exit");
 
         let mut option = String::new();
@@ -17,7 +18,10 @@ fn main() {
                 break;
             },
             "1" => {
-                temp_converter()
+                celsius_fahrenheit_converter()
+            },
+            "2" => {
+                nth_fibonacci_number()
             },
             _ => {
                 println!("Invalid option selected. Please try again.");
@@ -27,7 +31,52 @@ fn main() {
     }
 }
 
-fn temp_converter() {
+fn nth_fibonacci_number() {
+    let mut f: [u32; 2] = [1; 2];
+    loop {
+        println!("Find nth Fibonacci number");
+    
+        let mut n = String::new();
+    
+        println!("Enter n (1 <= n <= 47).");
+    
+        io::stdin()
+            .read_line(&mut n)
+            .expect("Failed to read line");
+    
+        let n: u32 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input. Please try again.");
+                continue;
+            },
+        };
+
+        if n <= 0 {
+            println!("Invalid input. Please try again.");
+            continue;
+        }
+
+        if n <= 2 {
+            println!("F({}) = 1", n);
+            break;
+        }
+
+        let mut counter = n;
+        
+        while counter > 2 {
+            counter -= 1;
+            let next = f[0]+f[1];
+            f[0] = f[1];
+            f[1] = next;
+        }
+
+        println!("F({}) = {}", n, f[1]);
+        break;
+    }
+}
+
+fn celsius_fahrenheit_converter() {
     loop {
         let mut unit = String::new();
         println!("Enter input unit(C/c=Celsius, F/f=Fahrenheit).");
