3f2ebfda8c18cb0d09f575419e85b5b6d85cd047
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-11 09:04:20 +0530

    feat: twelve days of christmas song and menu labels updated

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,26 @@ use std::io;
 
 fn main() {
     loop {
-        println!("exercises");
-        println!("1. temp. converter");
-        println!("2. fibonacci");
+        println!("Rust exercises on loops");
+        println!("1. Temperature converter from Celsius to Fahrenheit and vice versa");
+        println!("2. Find n-th Fibonacci number");
+        println!("3. Print \"Twelve days of Christmas\" song lyrics & exit");
         println!("0. Exit");
 
         let mut option = String::new();
         io::stdin()
             .read_line(&mut option)
             .expect("Failed to read line");
-        
         match option.trim() {
             "0" => {
                 println!("Bye!");
                 break;
-            },
-            "1" => {
-                celsius_fahrenheit_converter()
-            },
-            "2" => {
-                nth_fibonacci_number()
+            }
+            "1" => celsius_fahrenheit_converter(),
+            "2" => nth_fibonacci_number(),
+            "3" => {
+                twelve_days_of_christman();
+                break;
             },
             _ => {
                 println!("Invalid option selected. Please try again.");
@@ -31,25 +31,72 @@ fn main() {
     }
 }
 
+fn twelve_days_of_christman() {
+    let sent_items = [
+        "A patridge in a pear tree",
+        "Two turtle doves",
+        "Three french hens",
+        "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying",
+        "Seven swans a-swimming",
+        "Eight maids a-milking",
+        "Nine ladies dancing",
+        "Ten lords a-leaping",
+        "Eleven pipers piping",
+        "Twelve drummers drumming",
+    ];
+    let day_in_words = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eigth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+
+    let title = "The Twelve Days of Christmas lyrics";
+    
+    // print the title
+    let dashes = "=".repeat(title.len());
+    println!();
+    println!("{}", dashes);
+    println!("{}", title);
+    println!("{}", dashes);
+    println!();
+
+    // print the verses
+    for (i, day_word) in day_in_words.iter().enumerate() {
+        println!("[Verse {}]", i + 1);
+        println!(
+            "On the {} day of Christmas, my true love sent to me",
+            day_word
+        );
+
+        if i > 0 {
+            for i1 in (2 .. i + 1).rev() {
+                println!("{}", sent_items[i1]);
+            }
+
+            println!("{}, and", sent_items[1]);
+        }
+
+        println!("{}", sent_items[0]);
+
+        println!();
+    }
+}
+
 fn nth_fibonacci_number() {
     let mut f: [u32; 2] = [1; 2];
     loop {
         println!("Find nth Fibonacci number");
-    
         let mut n = String::new();
-    
         println!("Enter n (1 <= n <= 47).");
-    
-        io::stdin()
-            .read_line(&mut n)
-            .expect("Failed to read line");
-    
+
+        io::stdin().read_line(&mut n).expect("Failed to read line");
         let n: u32 = match n.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Invalid input. Please try again.");
                 continue;
-            },
+            }
         };
 
         if n <= 0 {
@@ -63,10 +110,9 @@ fn nth_fibonacci_number() {
         }
 
         let mut counter = n;
-        
         while counter > 2 {
             counter -= 1;
-            let next = f[0]+f[1];
+            let next = f[0] + f[1];
             f[0] = f[1];
             f[1] = next;
         }
