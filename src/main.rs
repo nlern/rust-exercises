1f7291c82812454e2537235eae778dc497352272
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-13 12:33:07 +0530

    feat: using enum with struct

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ enum IpAddrKind {
     V6,
 }
 
-fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-    route(four);
-    route(six);
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
 }
 
-fn route(ip_kind: IpAddrKind) {}
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+}
