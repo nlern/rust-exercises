9a229ad38f0cc348d92704dfd58efc731dbb9ce3
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-15 07:36:01 +0530

    exp: Option enum

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
-
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    let x: i8 = 5;
+    let y: Option<i8> = Some(6);
+
+    let sum = x + y;
 }
