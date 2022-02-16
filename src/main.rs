7630cdcf0466e3bcd9f2de67475a5d23352c62f5
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-16 08:39:13 +0530

    feat: catch all pattern

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky Penny!");
-            1
-        },
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        },
+fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
     }
 }
 
-fn main() {
-    value_in_cents(Coin::Quarter(UsState::Alaska));
-}
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
