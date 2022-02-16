447568476d41056ccbc0669bab0b30a0ca770fc0
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-16 09:01:10 +0530

    feat: if let pattern

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let dice_roll = 9;
-    match dice_roll {
-        3 => add_fancy_hat(),
-        7 => remove_fancy_hat(),
-        _ => (),
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
     }
 }
-
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
