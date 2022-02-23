3394bcf0c5f7e28eb0cd252a08de134ba3c46909
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-23 07:58:15 +0530

    feat: iterate over hashmap

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ fn main() {
   scores.insert(String::from("Blue"), 10);
   scores.insert(String::from("Yellow"), 50);
 
-  let team_name = String::from("Blue");
-  let score = scores.get(&team_name);
-
-  if let Some(value) = score {
-      println!("{}", value);
+  for (key, value) in &scores {
+      println!("{}: {}", key, value);
   }
 }
