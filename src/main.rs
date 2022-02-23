e56d7043c674bba97d53d68ade3b69402b2af104
Shantanu Dutta <external.dutta_shantanu@allianz.com>
2022-02-23 08:13:03 +0530

    feat: update hashmap key value based on old value

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::collections::HashMap;
 
 fn main() {
-  let mut scores = HashMap::new();
+  let text = "hello world wonderful world";
 
-  scores.insert(String::from("Blue"), 10);
-  
-  scores.entry(String::from("Yellow")).or_insert(50);
-  scores.entry(String::from("Blue")).or_insert(50);
+  let mut map = HashMap::new();
 
-  println!("{:?}", scores);
+  for word in text.split_whitespace() {
+      let count = map.entry(word).or_insert(0);
+      *count += 1;
+  }
+
+  println!("{:?}", map);
 }
