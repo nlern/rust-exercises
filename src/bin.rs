7f61e9a5f3bcd4e68177cb566a8e7b325f37d43d
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-03-18 10:31:28 +0530

    chore: remove unused code

--- src/bin.rs
+++ src/bin.rs
@@ -1,26 +1,4 @@
-use aggregator::{Summary, Tweet, NewsArticle, notify};
+
 
 fn main() {
-  let tweet = Tweet {
-    username: String::from("horse_books"),
-    content: String::from(
-      "of course, as you probably already know, people",
-    ),
-    reply: false,
-    retweet: false,
-  };
-
-  println!("1 new tweet: {}", tweet.summarize());
-
-  let article = NewsArticle {
-    headline: String::from("Penuins win the Stanley Cup Championship!"),
-    location: String::from("Pittsburgh, PA, USA"),
-    author: String::from("Iceburgh"),
-    content: String::from(
-      "The Pittsburgh Penguins once again are the best \
-       hockey team in the NHL.",
-    ),
-  };
-
-  notify(&article);
 }
