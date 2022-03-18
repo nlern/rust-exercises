1498924986badb26da73633c8ca3cf7f8dceae0c
Shantanu Dutta <connect.shantanu.dutta@gmail.com>
2022-03-18 12:24:25 +0530

    feat: lifetime for function parameters

--- src/bin.rs
+++ src/bin.rs
@@ -7,9 +7,11 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 fn main() {
-  let string1 = String::from("abcd");
-  let string2 = "xyz";
-
-  let result = longest(string1.as_str(), string2);
+  let string2 = String::from("xyz");
+  let string1 = String::from("long string is long");
+  let result;
+  {
+    result = longest(string1.as_str(), string2.as_str());
+  }
   println!("The longest string is {}", result);
 }
